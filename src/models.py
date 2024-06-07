c0cf52338d31c7068aa2af3bf8044d315683e880
James Dugard <161103530+jdugard3@users.noreply.github.com>
2024-06-07 01:51:42 +0000

    completed project

--- src/models.py
+++ src/models.py
@@ -22,9 +22,62 @@ class User(Base):
 class Character(Base):
     __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    character_name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
     birth_year = Column(String(250), nullable=False)
-    
+    home_world = Column(String(250), nullable=False)
+    films = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    species = Column(String(250), nullable=False)
+    vehicles_used = Column(String(250), nullable=False)
+    starships_flown = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Planet(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    population = Column(String(250))
+    gravity = Column(String(250))
+    terrain = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    residents = Column(String(250), nullable=False)
+    films = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    character = relationship(Character)
+
+class Vehicle(Base): 
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    vehicle_class = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    cost_in_credits = Column(String(250), nullable=False)
+    crew = Column(String(250), nullable=False)
+    max_speed = Column(String(250), nullable=False)
+    cargo_capacity = Column(String(250), nullable=False)
+    pilots = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Favorite(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    character = relationship(Character)
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    planet = relationship(Planet)
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    vehicle = relationship(Vehicle)
+
+
+
+
 
 
 # class Address(Base):
